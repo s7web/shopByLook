b6ab67e774a251db0d75316b3804566c0881e58f
s7njaric <njaric@s7designcreative.com>
2015-05-28 17:02:18 +0200

    refactored code

--- inc/views/archive.php
+++ inc/views/archive.php
@@ -1,9 +1,8 @@
 <?php
-get_header();
-global $woocommerce;
+include( 'header.php' );
 while ( have_posts() ) : the_post();
 	?>
-	<div class="shop_by_look archive">
+	<article class="shop_by_look archive">
 		<div class="left_column">
 			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<?php
@@ -29,21 +28,35 @@ while ( have_posts() ) : the_post();
 			<?php $ids = get_post_meta( $post->ID, 'products', TRUE ); ?>
 			<?php $products = new WP_Query( array( 'post_type' => 'product', 'post__in' => $ids ) ); ?>
 			<h3><?php _e( 'Products', 'shop_by_look' ); ?></h3>
-			<ul>
-				<?php while ( $products->have_posts() ): ?>
-					<li>
-						<?php $products->the_post(); ?>
-						<?php $p = new WC_Product( get_the_ID() );
-						echo $p->get_image( array( 30, 30 ) ); ?>
-						<a href="<?php echo $p->add_to_cart_url(); ?>"><?php the_title(); ?></a>
-						<span><?php echo $p->get_price_html(); ?></span>
-					</li>
-				<?php endwhile;
-				wp_reset_postdata(); ?>
-			</ul>
+			<table>
+				<?php
+				$total = 0;
+				while ( $products->have_posts() ):
+					?>
+					<tr>
+						<?php
+						$products->the_post();
+						$p = new WC_Product( get_the_ID() );
+						?>
+						<?php  ?>
+						<td><a href="<?php echo $p->add_to_cart_url(); ?>">
+							<?php
+							echo $p->get_image( array( 30, 30 ) ) . ' ';
+							the_title();
+							?>
+						</a></td>
+						<td><?php echo $p->get_price_html(); ?></td>
+					</tr>
+				<?php endwhile; ?>
+				<tr>
+					<td><b>Total:</b></td>
+					<td></td>
+				</tr>
+				<?php wp_reset_postdata(); ?>
+			</table>
 		</div>
-	</div>
+	</article>
 <?php
 endwhile;
-get_footer();
+include( 'footer.php' );
 ?>
--- inc/views/single.php
+++ inc/views/single.php
@@ -1,33 +1,42 @@
-<?php get_header(); ?>
-<?php global $woocommerce; ?>
-<div class="shop_by_look">
+<?php include( 'header.php' ); ?>
+<article class="shop_by_look">
 	<div class="left_column">
 		<h1><?php the_title(); ?></h1>
-		<?php the_post_thumbnail( 'full' ); ?>
+		<?php the_post_thumbnail( 'small' ); ?>
 		<?php the_content(); ?>
 	</div>
 	<div class="right_column">
 		<?php $ids = get_post_meta( $post->ID, 'products', TRUE ); ?>
 		<?php $products = new WP_Query( array( 'post_type' => 'product', 'post__in' => $ids ) ); ?>
-		<h2><?php _e( 'Products', 'shop_by_look' ); ?></h2>
-		<div class="links">
-			<a data-shop-all href="#" class="buy-all button"><?php _e( 'Buy all', 'shop_by_look' ); ?></a>
-			<a class="cart"
-			   href="<?php echo $woocommerce->cart->get_cart_url(); ?>"><?php _e( 'Cart', 'shop_by_look' ); ?></a>
-		</div>
-		<ul>
+		<h3><?php _e( 'Products', 'shop_by_look' ); ?></h3>
+		<table>
+			<tr>
+				<td><button data-shop-all class="buy-all button"><?php _e( 'Buy all', 'shop_by_look' ); ?></button></td>
+				<td colspan="2">
+					<a class="cart"
+					   href="<?php echo $woocommerce->cart->get_cart_url(); ?>">
+						<?php _e( 'View cart', 'shop_by_look' ); ?>
+					</a>
+				</td>
+			</tr>
 			<?php while ( $products->have_posts() ): ?>
-				<li>
+				<tr>
 					<?php $products->the_post(); ?>
-					<?php $p = new WC_Product( get_the_ID() );
-					echo $p->get_image( array( 30, 30 ) ); ?>
-					<a href="<?php echo $p->add_to_cart_url(); ?>"><?php the_title(); ?></a>
-					<span><?php echo $p->get_price_html(); ?></span>
-					<a data-shop="<?php the_ID(); ?>" class="button add-to-cart" href="#" title="Add to cart">+</a>
-				</li>
+					<?php $p = new WC_Product( get_the_ID() ); ?>
+					<td>
+						<a href="<?php echo $p->add_to_cart_url(); ?>">
+							<?php
+							echo $p->get_image( array( 30, 30 ) ) . ' ';
+							the_title();
+							?>
+						</a>
+					</td>
+					<td><?php echo $p->get_price_html(); ?></td>
+					<td><a data-shop="<?php the_ID(); ?>" class="button add-to-cart" href="#" title="Add to cart">+</a></td>
+				</tr>
 			<?php endwhile;
 			wp_reset_postdata(); ?>
-		</ul>
+		</table>
 	</div>
-</div>
-<?php get_footer(); ?>
+</article>
+<?php include( 'footer.php' ); ?>
